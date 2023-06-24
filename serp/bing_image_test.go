d8bd99fb9640c3552d837ca4643283be930447d8
ikechan8370 <geyinchi@buaa.edu.cn>
2023-06-24 17:33:25 +0800

    fix: error

--- serp/bing_image_test.go
+++ serp/bing_image_test.go
@@ -3,7 +3,7 @@ package serp
 import "testing"
 
 func TestSearchBingImage(t *testing.T) {
-	bing, err := SearchBingImage(nil, "猫", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36", 10, "")
+	bing, err := SearchBingImage(nil, "cat", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36", 10, "", false)
 	if err != nil {
 		return
 	}
